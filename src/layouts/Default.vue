bddcceff40c469c5e8e57306e903a9fe43c0fce4
chr1sw <cwoehrle@gmail.com>
2019-12-17 15:48:18 -0500

    Initial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header>
+        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+      <nav>
+        <g-link to="/">Home</g-link>
+        <g-link to="/about/">About</g-link>
+      </nav>
+    </header>
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
